3709243e2b1a234be7116317c069ce706febdfcd
AlexBlagodatskikh <6219286a@gmail.com>
2026-05-19 23:30:00 +0300

    Add Календарь "Фенек"

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ const translations = {
       'Печатная графика': 'Печатная графика'
     },
     projects: {
+      n99170: { title: "Календарь "Фенек"", desc: "Отрисовано 12 иллюстраций фенька на каждый месяц" },
       pwg24m: { title: "Календарь "Фенек"", desc: "Отрисовано 12 иллюстраций фенька на каждый месяц" },
       p0462: { title: "Календарь "Фенек"", desc: "Отрисовано 12 иллюстраций на каждый месяц с милым феньком" },
       f1: { title: "AUTOHUB - социальная сеть для автолюбителей (сайт)", desc: "Прототип социальной сети для автолюбителей в формате вайрфреймов и финальном виде." },
@@ -159,6 +160,7 @@ const translations = {
       'Печатная графика': 'Print Graphics'
     },
     projects: {
+      n99170: { title: "Fennec calendar", desc: "12 illustrations of fennec were drawn for each month" },
       pwg24m: { title: "Fennec calendar", desc: "12 illustrations of fennec were drawn for each month" },
       p0462: { title: "Fennec calendar", desc: "12 illustrations drawn for each month with a cute fennec" },
       f1: { title: "AUTOHUB - social network for car enthusiasts (website)", desc: "Social network prototype for car enthusiasts in wireframe and final format." },
@@ -288,6 +290,8 @@ const App = () => {
       { id: 'f6', cat: "Веб-дизайн",       year: 2025, img: "images/избранное/лонгрид/1.png",    pdf: "images/избранное/лонгрид/2.pdf" },
     ],
     archive: [
+      { id: 'n99170', cat: "Печатная графика", year: 2026, img: "images/xxxxxxxxx--xxxxx-/1.png", pdf: "images/xxxxxxxxx--xxxxx-/2.pdf" },
+
 { id: 'f3', cat: "Веб-дизайн",       year: 2025, img: "images/избранное/1.png",            pdf: "images/избранное/mir/картина мира.pdf" },
       { id: 'pwg24m', cat: "Печатная графика", year: 2026, img: "images/xxxxxxxxx--xxxxx-/1.png", pdf: "images/xxxxxxxxx--xxxxx-/2.pdf" },
 
